40e7d611f75f9dee5d53ce823a009f720fb639be
minkyukim <mok07333@gmail.com>
2026-02-16 14:55:13 +0900

    - modify small things

--- inc/core/systemDefs.h
+++ inc/core/systemDefs.h
@@ -8,36 +8,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-// Option Propagation: defaults only if not defined by appConfig.h
-#ifndef APP_OS
-    #define APP_OS 0
-#endif
-#ifndef APP_LOG_ENABLE
-    #define APP_LOG_ENABLE 0
-#endif
-#ifndef APP_LOG_LEVEL
-    #define APP_LOG_LEVEL 0
-#endif
-#ifndef SYSTEM_LOG_LEVEL_ERROR
-    #define SYSTEM_LOG_LEVEL_ERROR 1
-#endif
-#ifndef SYSTEM_LOG_LEVEL_WARN
-    #define SYSTEM_LOG_LEVEL_WARN 2
-#endif
-#ifndef SYSTEM_LOG_LEVEL_INFO
-    #define SYSTEM_LOG_LEVEL_INFO 3
-#endif
-#ifndef SYSTEM_LOG_LEVEL_DEBUG
-    #define SYSTEM_LOG_LEVEL_DEBUG 4
-#endif
-#ifndef SYSTEM_LOG_LEVEL_NONE
-    #define SYSTEM_LOG_LEVEL_NONE 5
-#endif
-
-#if APP_OS == OS_LINUX
-    #define _GNU_SOURCE
-#endif
-
 typedef enum{
     retOk = 0,
     retFail = -1,
